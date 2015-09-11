6792eda8d41bbd72eb7c5c8b4c38b3a06c892c10
Miguel Saiz <miguelsaizo@gmail.com>
2015-09-11 16:07:20 -0400

    Adds the animation when changing

--- Example/CustomCheckbox/ViewController.swift
+++ Example/CustomCheckbox/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import CustomCheckbox
 
 class ViewController: UIViewController {
+  @IBOutlet weak var checkbox: Checkbox!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+      checkbox.setCheckedImage(UIImage(named: "checked")!)
+      checkbox.setUncheckedImage(UIImage(named: "unchecked")!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+  @IBAction func pressedCheckbox(sender: AnyObject) {
+    print("pressed the checkbox")
+  }
 }
+
+
 
--- Pod/Classes/Checkbox.swift
+++ Pod/Classes/Checkbox.swift
@@ -8,14 +8,73 @@
 
 import UIKit
 
-class Checkbox: UIControl {
+public protocol CheckboxDelegate {
+  func canCheck(enabled: Bool)
+  func checked(checkbox: Checkbox)
+  func unchecked(checkbox: Checkbox)
+}
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+
+public class Checkbox: UIControl {
+  
+  internal var checkedImageView = UIImageView()
+  internal var uncheckedImageView = UIImageView()
+  public var scaleFactor: CGFloat = 1.2
+  public var animationDuration: NSTimeInterval = 0.1
+  public var delegate: CheckboxDelegate?
+
+  init() {
+    super.init(frame: CGRectZero)
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  public func setCheckedImage(image: UIImage) {
+    checkedImageView.image = image
+    checkedImageView.alpha = 0
+    addSubview(checkedImageView)
+  }
+  
+  public func setUncheckedImage(image: UIImage) {
+    uncheckedImageView.image = image
+    addSubview(uncheckedImageView)
+  }
+  
+  override public func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+    
+    transform = CGAffineTransformIdentity
+    UIView.animateWithDuration(animationDuration) { () -> Void in
+      self.transform = CGAffineTransformMakeScale(self.scaleFactor, self.scaleFactor)
     }
-    */
+    
+    return true
+  }
+  
+  public override func endTrackingWithTouch(touch: UITouch?, withEvent event: UIEvent?) {
+    
+    if let point = touch?.locationInView(self) where pointInside(point, withEvent: nil) {
+      selected = !selected
+      
+      delegate?.checked(self)
+      UIView.animateWithDuration(animationDuration) { () -> Void in
+        self.transform = CGAffineTransformIdentity
+        self.checkedImageView.alpha = self.selected ? 1 : 0
+        self.uncheckedImageView.alpha = self.selected ? 0 : 1
+      }
+      
+    } else {
+      delegate?.unchecked(self)
+      UIView.animateWithDuration(animationDuration) { () -> Void in
+        self.transform = CGAffineTransformIdentity
+      }
+    }
+  }
+  
+  override public func layoutSubviews() {
+    checkedImageView.frame = bounds
+    uncheckedImageView.frame = bounds
+  }
 
 }
