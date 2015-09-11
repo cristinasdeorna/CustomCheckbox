a094161e9e4d29c3f9c25028df81ee2e9cb8674e
Miguel Saiz <miguelsaizo@gmail.com>
2015-09-11 11:13:53 -0400

    Initial commit

---
+++ Example/CustomCheckbox/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CustomCheckbox
+//
+//  Created by Miguel Saiz on 09/11/2015.
+//  Copyright (c) 2015 Miguel Saiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
